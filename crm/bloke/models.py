b7be58f7060b244c4dacce8f702dd7fbbad6d579
Chandrasekaraa Ramadoss Ganesh <94929823+RGChandrasekaraa@users.noreply.github.com>
2024-07-31 22:23:22 +1000

    Update models.py

--- crm/bloke/models.py
+++ crm/bloke/models.py
@@ -3,6 +3,7 @@ from django.db import models
 class Record(models.Model):
     creation_date = models.DateTimeField(auto_now_add=True)
     first_name = models.CharField(max_length=100)
+    middle_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
     email = models.EmailField()
     phone = models.CharField(max_length=15)
